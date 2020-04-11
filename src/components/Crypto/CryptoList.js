d92b4092872feea668d0535a19deac8d01c6867a
StanGirard <girard.stanislas@gmail.com>
2020-04-11 22:22:27 +0200

    [CRYPTO] Percentage Change

--- src/components/Crypto/CryptoList.js
+++ src/components/Crypto/CryptoList.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import "./crypto.css"
+import arrow_up from '../../assets/up-arrow.png'
+import arrow_down from '../../assets/down-arrow.png'
 
 const price_color = (value) => {
+
     return (
         <div className="CryptoPrice">
-            € {value["current_price"]}
+            <div className="CryptoIconPrice">
+                <div className="CryptoChangeIcon">
+                    <img src={value["price_change_24h"] > 0 ? arrow_up : arrow_down} alt='Price Change Logo' />
+                </div>
+                € {value["current_price"]}
+            </div>
+            <div className="CryptoPercentageChange">
+                <div className="row" >
+                    1h: <div className={value["price_change_percentage_1h_in_currency"] > 0 ? "green" : "red"}> {value["price_change_percentage_1h_in_currency"].toFixed(2)}% </div>
+                </div>
+                <div className="row" >
+                    24h: <div className={value["price_change_percentage_24h_in_currency"] > 0 ? "green" : "red"}> {value["price_change_percentage_24h_in_currency"].toFixed(2)}% </div>
+                </div>
+                <div className="row" >
+                    7d: <div className={value["price_change_percentage_7d_in_currency"] > 0 ? "green" : "red"}> {value["price_change_percentage_7d_in_currency"].toFixed(2)}% </div>
+                </div>
+
+            </div>
         </div>
+
     )
+
 }
 
 const CryptoList = ({ value }) => (
     <div className="CryptoElement">
+        {/* Image and Name of the Crypto */}
         <div className="CryptoHead">
             <div className="CryptoImage">
                 <img src={value["image"]} alt="Logo" />
@@ -19,12 +42,21 @@ const CryptoList = ({ value }) => (
                 {value["name"]}
             </div>
         </div>
-        <div className="CryptoDivider" />
+        {/* Generates the Price with the right color */}
         {price_color(value)}
-        
-        {/* <div className="CryptoPrice">
-            {value["current_price"]}
+        {/* Generates the high&low value in the last 24h */}
+        {/* <div className="HighLow24h">
+            <div className="HighLow24hTitle">
+                24H
+            </div>
+            <div className="High24h">
+                <img src={arrow_up} /> {value["high_24h"]}
+            </div>
+            <div className="Low24h">
+                <img src={arrow_down} /> {value["low_24h"]}
+            </div>
         </div> */}
+
     </div>
 )
 
--- src/sagas/bitcoinsaga.js
+++ src/sagas/bitcoinsaga.js
@@ -17,7 +17,7 @@ export function* getBitcoin({ }) {
     console.log("HELLO")
     const response = yield call(
       request,
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false`,
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y`,
     );
     console.log(response)
     yield put({
